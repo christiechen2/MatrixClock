2770beb987ad0f60a09dfd7c092edd95085f5b23
Leonardo Poletti Atalla <leonardo@atalla.org>
2023-11-16 21:26:30 -0500

    test

--- update_daemon.sh
+++ update_daemon.sh
@@ -2,14 +2,14 @@
 
 # Function to run the Python program with a timeout of 60 seconds
 run_python_program() {
-    timeout 5 python your_python_script.py
+    timeout 5 python update_design.py
     # Replace 'your_python_script.py' with the actual name of your Python script
 }
 
 # Function to pull changes from a GitHub repository
 pull_from_github() {
     # Replace 'username/repo' with your GitHub username and repository name
-    git -C /path/to/your/repo/clone pull origin main
+    git pull origin main
 }
 
 # Main loop
