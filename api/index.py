b526bfafb7fb25e0d51c768ff4239e34a6b2913d
Leonardo Poletti Atalla <leonardo@atalla.org>
2023-11-16 20:23:50 -0500

    add logic to get designs

--- api/index.py
+++ api/index.py
@@ -5,13 +5,30 @@ app = Flask(__name__)
 
 
 # Define a route that returns JSON data
-@app.route('/api/data', methods=['GET'])
-def get_data():
+@app.route('/current_design', methods=['GET'])
+def get_daily():
     data = {
-        'message': 'Hello, World!',
-        'number': 42
+        'designs': 'sample_design',
+        'settings': {
+            'led-rows': 64,
+            'led-cols': 64,
+            'led-slowdown-gpio': 4,
+            'led-brightness': 20,
+        }
     }
     return jsonify(data)
+
+
+@app.route('/design/<design>', methods=['GET'])
+def get_design(design):
+    # get design file from designs/design_name
+    # return design file
+    try:
+        file = open("api/designs/" + design + ".py", "r")
+        return file.read()
+    except FileNotFoundError:
+        return "Design not found"
+
 
 @app.route('/', methods=['GET'])
 def get_root():
