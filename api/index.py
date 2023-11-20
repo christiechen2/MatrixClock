0f1a788885606e238499d46a4fa4a297961b55e7
Leonardo Poletti Atalla <leonardo@atalla.org>
2023-11-20 17:51:50 -0500

    basic running text

--- api/index.py
+++ api/index.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/current_design', methods=['GET'])
 def get_daily():
     data = {
-        'design': 'sample_design',
+        'design': 'running_text',
         'settings': {
             'led-rows': 64,
             'led-cols': 64,
